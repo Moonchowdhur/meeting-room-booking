facbc0ff6328c7f415cf739a18f61344779ada93
kaushani <kaushanichowdhury52@gmail.com>
2024-08-28 14:19:01 +0530

    add

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,7 @@ import notFound from './app/middleware/notFound';
 const app: Application = express();
 
 app.use(express.json());
-// app.use(cors());
-// app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+
 // app.use(cors());
 
 app.use(cors({ origin: true, credentials: true }));
