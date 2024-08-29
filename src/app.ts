55525299d50e0480d6598f9a6e0ffc6d0278492c
kaushani <kaushanichowdhury52@gmail.com>
2024-08-29 08:30:18 +0530

    added

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,13 @@ const app: Application = express();
 app.use(express.json());
 
 // app.use(cors());
+// app.use(
+//   cors({ origin: 'https://room-booking-redux.vercel.app', credentials: true }),
+// );
 
-app.use(cors({ origin: true, credentials: true }));
+// app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+
+app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
 
 app.use(express.text());
 
